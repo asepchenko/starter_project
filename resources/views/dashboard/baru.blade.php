179efd771daf5e25306df1fd0acf91917c7ff482
asepchenko <ache.depok@gmail.com>
2023-04-25 00:41:36 +0700

    selesai bagian 6, fix starter sebelum middleware

---
+++ resources/views/dashboard/baru.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.app')
+
+@section('title','Baru')
+
+@section('content')
+    <section class="section">
+    <div class="section-header">
+      <h1>Halaman Baru</h1>
+
+    </div>
+
+    <div class="section-body">
+      Content Here
+    </div>
+  </section>
+@endsection
+
+@section('sidebar')
+
+@endsection
--- resources/views/layouts/custom.blade.php
+++ resources/views/layouts/custom.blade.php
@@ -18,24 +18,12 @@
 
 <body>
   <div id="app">
-    <div class="main-wrapper">
-      <div class="navbar-bg"></div>
-      <nav class="navbar navbar-expand-lg main-navbar">
-        @include('layouts.navbar')
-      </nav>
-      <div class="main-sidebar sidebar-style-2">
-        @include('layouts.sidebar')
-      </div>
+    <section class="section">
+        <div class="container mt-5">
+            @yield('content')
+        </div>
 
-      <!-- Main Content -->
-      <div class="main-content">
-        @yield('content')
-      </div>
-
-      <footer class="main-footer">
-        @include('layouts.footer')
-      </footer>
-    </div>
+    </section>
   </div>
 
   <!-- General JS Scripts -->
